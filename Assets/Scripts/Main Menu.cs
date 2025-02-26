94e94d07107cb94570b3cc000373aa61182d3538
kf496 <kf496@student.aru.ac.uk>
2025-02-26 12:05:56 +0000

    Added ability to stop running in editor and comments added

--- Assets/Scripts/Main Menu.cs
+++ Assets/Scripts/Main Menu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,5 +9,11 @@ public class MainMenu : MonoBehaviour
 public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //loads next scene in queue
+    }
+public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit(); //Closes app
+        EditorApplication.isPlaying = false; //Stops running in Unity Editor
     }
 }
